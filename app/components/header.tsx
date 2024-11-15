f4d28a9a708c66d3a8b418da22f0d6e358fe489e
alisbah7 <alishbah04ak@gmail.com>
2024-11-15 10:07:28 +0500

    Porfolio With Custom CSS

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,31 +1,36 @@
 'use client';
+import { useEffect } from 'react';
+import AOS from 'aos';
 import Link from 'next/link'
 import  '../../public/css/header.css'
 
 
 export default function Header(){
+    useEffect(() => {
+        AOS.init({ duration: 2000, easing: 'ease', delay: 200 });
+  }, []);
     return (
         <div>
             <header>
             <h1 className='logo'>ALISHBAH</h1>
 
 
-                <input type="checkbox" id="check" />
-                <label htmlFor="check" className="icon">
-                    <i className="bx bx-menu" id="menu-icon"></i>
-                    <i className="bx bx-x" id="close-icon"></i>
+                <input type='checkbox' id='check' />
+                <label htmlFor='check' className='icon'>
+                    <i className='bx bx-menu' id='menu-icon'></i>
+                    <i className='bx bx-x' id='close-icon'></i>
                 </label>
 
-                <nav className="navbar">
-                    <Link href="#" className="nav-item" style={{'--i': 0} as React.CSSProperties} data-aos="fade-down" data-aos-duration="2000">Home</Link>
-                    <Link href="#" className="nav-item" style={{'--i': 1} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
-                    <Link href="#" className="nav-item" style={{'--i': 2} as React.CSSProperties} data-aos="fade-down" data-aos-duration="2000">About</Link>
-                    <Link href="#" className="nav-item" style={{'--i': 3} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
-                    <Link href={''} className="nav-item" style={{'--i': 4} as React.CSSProperties} id='logo' data-aos="flip-down" data-aos-duration="2000" data-aos-delay='300'><h1>ALISHBAH</h1></Link>
-                    <Link href="#" className="nav-item" style={{'--i': 5} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
-                    <Link href="#" className="nav-item" style={{'--i': 6} as React.CSSProperties} data-aos="fade-down" data-aos-duration="2000">Projects</Link>
-                    <Link href="#" className="nav-item" style={{'--i': 7} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
-                    <Link href="#" className="nav-item" style={{'--i': 8} as React.CSSProperties}data-aos="fade-down" data-aos-duration="2000">Contact</Link>
+                <nav className='navbar'>
+                    <Link href='#' className='nav-item' style={{'--i': 0} as React.CSSProperties} data-aos='fade-down'>Home</Link>
+                    <Link href='#' className='nav-item' style={{'--i': 1} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
+                    <Link href='#' className='nav-item' style={{'--i': 2} as React.CSSProperties} data-aos='fade-down'>About</Link>
+                    <Link href='#' className='nav-item' style={{'--i': 3} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
+                    <Link href={''} className='nav-item' style={{'--i': 4} as React.CSSProperties} id='logo' data-aos='flip-down'><h1>ALISHBAH</h1></Link>
+                    <Link href='#' className='nav-item' style={{'--i': 5} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
+                    <Link href='#' className='nav-item' style={{'--i': 6} as React.CSSProperties} data-aos='fade-down'>Projects</Link>
+                    <Link href='#' className='nav-item' style={{'--i': 7} as React.CSSProperties} id='circleIcon'><i className='bx bxs-circle'></i></Link>
+                    <Link href='#' className='nav-item' style={{'--i': 8} as React.CSSProperties} data-aos='fade-down'>Contact</Link>
                 </nav>
 
             </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
 // import Image from 'next/image';
+import { useEffect } from 'react';
+import AOS from 'aos';
 import Link from 'next/link';
 import Header from './components/header';
 import '../public/css/hero.css'
 
 export default function Home() {
+  useEffect(() => {
+      AOS.init({ duration: 2000, easing: 'ease', delay: 0 });
+}, []);
   return (
     <div>
 
@@ -12,9 +18,9 @@ export default function Home() {
 
 
       {/*------====== HERO & ABOUT ======------*/}
-      <div className="mainPart"> 
-        <div className="mainSec"> 
-          <div className="container">
+      <div className='mainPart'> 
+        <div className='mainSec'> 
+          <div className='container'>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto dolore harum veritatis pariatur commodi sed ipsa, illo aut sunt amet iste vero, dignissimos labore a repudiandae, error nihil debitis recusandae est consectetur tempora ipsam. Rem ipsum facilis veritatis ea! Eveniet autem debitis quae, quibusdam ex natus sapiente ad rerum?</p><br />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto dolore harum veritatis pariatur commodi sed ipsa, illo aut sunt amet iste vero, dignissimos labore a repudiandae, error nihil debitis recusandae est consectetur tempora ipsam. Rem ipsum facilis veritatis ea! Eveniet autem debitis quae, quibusdam ex natus sapiente ad rerum?</p><br />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto dolore harum veritatis pariatur commodi sed ipsa, illo aut sunt amet iste vero, dignissimos labore a repudiandae, error nihil debitis recusandae est consectetur tempora ipsam. Rem ipsum facilis veritatis ea! Eveniet autem debitis quae, quibusdam ex natus sapiente ad rerum?</p><br />
@@ -33,16 +39,16 @@ export default function Home() {
           </div>
 
           <div className='heroSec'>
-            <div className='bg' data-aos="zoom-out-right" data-aos-duration="2000">
+            <div className='bg' data-aos='zoom-out-right'>
               <div id='heroPt1'>
                 <h1>I'm <i>ALISHBAH</i></h1>
                 <h4>A Full Stack Web Developer</h4>
-                <div id='heroBtn'><button className='gradient-border'><Link href="" id='heroBtn'>Explore Projects</Link></button></div>
+                <div id='heroBtn'><button className='gradient-border'><Link href='' id='heroBtn'>Explore Projects</Link></button></div>
               </div>
               <div id='socialMedia'>
-                <div className="social"><i className='bx bxl-linkedin'></i></div>
-                <div className="social2"><i className='bx bxl-github'></i></div>
-                <div className="social3"><i className='bx bxl-instagram' ></i></div>
+                <div className='social'><i className='bx bxl-linkedin'></i></div>
+                <div className='social2'><i className='bx bxl-github'></i></div>
+                <div className='social3'><i className='bx bxl-instagram' ></i></div>
               </div>
             </div>
           </div>
